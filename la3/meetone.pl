f722a2c13dacb5236d0d08596d0b52ef7961aac9
Harrison Jansen van Beek <harrisonjansenva@u.boisestate.edu>
2025-11-10 19:03:47 -0700

    add expected files for testing for ta2

--- la3/meetone.pl
+++ la3/meetone.pl
@@ -3,6 +3,26 @@
 :- include('data.pl').
 
 % Your code goes here.
+meetone(Person, MeetingSlot) :-
+	free(Person, FreeSlot),
+	overlap(FreeSlot, MeetingSlot).
+
+overlap(slot(start1, end1), slot(start2, end2)) :-
+	startAvailability :- (lte(time(start1), time(start2))),
+	endAvailability :- (lte(time(end1), time(end2)));
+
+lte(time1(h1, m1, d1), time2(h2, m2, d2)) :-
+	
+	
+
+
+	
+
+
+
+
+
+
 
 main :- findall(Person,
 		meetone(Person,slot(time(8,30,am),time(8,45,am))),
