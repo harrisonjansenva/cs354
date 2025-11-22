5bc64404f3c7cb51db3e90856bcce6bd227670a7
Harrison Jansen van Beek <harrisonjansenva@u.boisestate.edu>
2025-11-21 23:15:46 -0700

    update more functionality of la4

--- la4/CheckingAccount.go
+++ la4/CheckingAccount.go
@@ -1,1 +1,21 @@
 package bank
+
+type CheckingAccountInterface interface {
+	AccountInterface
+}
+
+type CheckingAccount struct {
+	Account
+}
+
+func NewCheckingAccount(name string, number string, balance float32) *CheckingAccount {
+	return &CheckingAccount{
+		Account: Account{
+			customer: Customer{
+				name: name,
+			},
+			number:  number,
+			balance: balance,
+		},
+	}
+}
--- la4/SavingsAccount.go
+++ la4/SavingsAccount.go
@@ -1,1 +1,29 @@
 package bank
+
+type SavingsAccountInterface interface {
+	AccountInterface
+}
+
+type SavingsAccount struct {
+	Account
+	interest float32
+}
+
+func NewSavingsAccount(name string, number string, balance float32) *SavingsAccount {
+	return &SavingsAccount{
+		Account: Account {
+			customer: Customer {
+				name: name,
+			},
+			number: number,
+			balance: balance,
+		},
+		interest: 0,
+	}
+}
+
+func (a *SavingsAccount) Accrue(rate float32) {
+	a.interest += a.balance * rate
+	a.balance += a.balance * rate
+
+}
